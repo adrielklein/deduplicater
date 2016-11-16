a5f1c5c7f5a4ff7df5d815cda2f75b6fd34db8d3
Adriel Klein <adrielmklein@gmail.com>
2016-11-16 12:59:08 -0800

    stop using graph

--- src/deduplication/Deduplicater.java
+++ src/deduplication/Deduplicater.java
@@ -1,75 +1,70 @@
 package deduplication;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Stack;
-
-class Node {
-    Record record;
-    ArrayList<Node> neighbors = new ArrayList<Node>();
-
-    public Node(Record newRecord) {
-        record = newRecord;
-    }
-
-    public void addNeighbor(Node node) {
-        neighbors.add(node);
-    }
-}
+import java.util.*;
 
 
 public class Deduplicater {
 
 
     public static DeduplicationResult getDeduplicationResult(ArrayList<Record> records) {
-        Stack<Node> nodes = getNodes(records);
+        Collections.sort(records);
 
         ArrayList<Record> uniqueRecords = new ArrayList<Record>();
         ArrayList<Change> changes = new ArrayList<Change>();
-        findUniqueRecords(nodes, uniqueRecords, changes);
+        findUniqueRecords(records, uniqueRecords, changes);
         Collections.sort(uniqueRecords);
 
         return new DeduplicationResult(uniqueRecords, changes);
     }
 
-    private static void findUniqueRecords(Stack<Node> nodes, ArrayList<Record> uniqueRecords, ArrayList<Change> changes) {
-        HashSet<Node> removedNodes = new HashSet<Node>();
-        while (!nodes.empty()) {
-            Node node = nodes.pop();
-            uniqueRecords.add(node.record);
-            removeNeighbors(nodes, changes, removedNodes, node);
+    private static void findUniqueRecords(ArrayList<Record> records, ArrayList<Record> uniqueRecords, ArrayList<Change> changes) {
+        HashMap[] maps = getMaps(records);
+        HashMap<String, ArrayList<Integer>> idToIndexes = maps[0];
+        HashMap<String, ArrayList<Integer>> emailToIndexes = maps[1];
+        boolean[] isVisited = new boolean[records.size()];
+        for (int index = records.size() - 1; index >= 0; index--) {
+            if (isVisited[index])
+                continue;
+            isVisited[index] = true;
+
+            Record record = records.get(index);
+            uniqueRecords.add(record);
+            visitDuplicates(records, changes, idToIndexes, isVisited, index, record.id);
+            visitDuplicates(records, changes, emailToIndexes, isVisited, index, record.email);
         }
+
     }
 
-    private static void removeNeighbors(Stack<Node> nodes, ArrayList<Change> changes, HashSet<Node> removedNodes, Node node) {
-        for (Node neighbor : node.neighbors) {
-            if (!removedNodes.contains(neighbor)) {
-                changes.add(new Change(neighbor.record, node.record));
-                nodes.removeElement(neighbor);
-                removedNodes.add(neighbor);
+    private static void visitDuplicates(ArrayList<Record> records, ArrayList changes, HashMap<String, ArrayList<Integer>> map, boolean[] isVisited, int index, String key) {
+        for (Integer duplicateIndex : map.get(key)) {
+            if (!duplicateIndex.equals(index) && !isVisited[duplicateIndex]) {
+                isVisited[duplicateIndex] = true;
+                changes.add(new Change(records.get(duplicateIndex), records.get(index)));
             }
         }
+        map.remove(key);
     }
 
 
-    private static Stack<Node> getNodes(ArrayList<Record> records) {
-        Collections.sort(records);
-        Stack<Node> nodes = new Stack<Node>();
-        for (Record record : records) {
-            nodes.push(new Node(record));
+    private static HashMap[] getMaps(ArrayList<Record> records) {
+        HashMap<String, ArrayList<Integer>> idToIndexes = new HashMap<String, ArrayList<Integer>>();
+        HashMap<String, ArrayList<Integer>> emailToIndexes = new HashMap<String, ArrayList<Integer>>();
+        for (int index = 0; index < records.size(); index++) {
+            Record record = records.get(index);
+            addToMap(record.id, index, idToIndexes);
+            addToMap(record.email, index, emailToIndexes);
         }
-        setEdges(nodes);
-        return nodes;
+        return new HashMap[]{idToIndexes, emailToIndexes};
+
     }
 
-    private static void setEdges(Stack<Node> nodes) {
-        for (Node firstNode : nodes) {
-            for (Node secondNode : nodes) {
-                if (firstNode != secondNode && firstNode.record.isDuplicate(secondNode.record))
-                    firstNode.addNeighbor(secondNode);
-            }
+    private static void addToMap(String key, int index, HashMap<String, ArrayList<Integer>> map) {
+        if (map.containsKey(key)) {
+            map.get(key).add(index);
+        } else {
+            ArrayList<Integer> indexes = new ArrayList<Integer>();
+            indexes.add(index);
+            map.put(key, indexes);
         }
     }
-
 }
